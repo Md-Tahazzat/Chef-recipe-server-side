390d2533312b369c32c1f57abb2d208b7859333f
Md-Tahazzat <mdtahazzatali820@gmail.com>
2023-05-01 21:13:43 +0600

    dynamic routes added

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const chefData = require("./data.json");
 
 const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Yummy Yums projects running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefData);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const mathchedData = chefData.find((chef) => chef.id == id);
+  res.send(mathchedData);
+  console.log(mathchedData);
 });
 
 app.listen(port, () => {
